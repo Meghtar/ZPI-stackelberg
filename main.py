45fe25a5c0812a0c6ba5389d195ccad4358cc41e
Meghtar <maciej.pypec@gmail.com>
2021-11-17 22:50:12 +0100

    Create basics of player classes

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello world')
+
+class Player:
+    def __init__(self, initial_strategy):
+        self._strategy = initial_strategy
+    
+    def get_strategy(self):
+        return self._strategy
+
+class Leader(Player):
+    pass
+
+class Follower(Player):
+    pass
+
+if __name__ == "__main__":
+    leader = Leader([1, 0, 0, 1])
+    follower = Follower([0, 1, 1, 0])
+
+    print("initial leader strategy: ", leader.get_strategy())
+    print("initial follower strategy: ", follower.get_strategy())
