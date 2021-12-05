e3043dd814a7708c496445483392561006578e10
Meghtar <maciej.pypec@gmail.com>
2021-12-05 18:23:10 +0100

    Basics of calculating payoff

--- Game.py
+++ Game.py
@@ -9,10 +9,34 @@ class Game:
         self.attack_probabilities = attack_probabilities
         self.round_counter = 0
     
+    def sigma(self, i):
+        return 1
+
     def calculate_leader_payoff(self):
+        total_payoff = 0
+        attack_count = 5
+        defence_count = 5
+        for i in range(attack_count):
+            for j in range(defence_count):
+                # sigma_i - strategia gracza (followersa/leadera) wobec i-tego elementu 
+                total_payoff += self.sigma(j) * self.sigma(i) * \
+                    (self.prob(attacks[i], countermeasures[j]) * \
+                    ((-1) * Val_a - CostDef(a,a,c,j))) + \
+                    (1 -self.prob(attacks[i], countermeasures[j])) * \
+                    (Val_a - CostDef(a,a,c,j)) # TODO: insert all necessary data and functions
         return 0
     
     def calculate_follower_payoff(self):
+        total_payoff = 0
+        attack_count = 5
+        defence_count = 5
+        for i in range(attack_count):
+            for j in range(defence_count):
+                total_payoff += self.sigma(j) * self.sigma(i) * \
+                    (self.prob(attacks[i], countermeasures[j]) * \
+                    (Gain(a) - CostAttack(a,a,c,i))) + \
+                    (1 -self.prob(attacks[i], countermeasures[j])) * \
+                    ((-1) * CostAttack(a,a,c,i)) # TODO: insert all necessary data and functions
         return 0
     
     def realise_round(self):
